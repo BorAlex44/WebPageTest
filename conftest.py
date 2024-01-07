fca8043c3950c7c86901a8776490200b3e612d13
BorAlex <alexander44@bk.ru>
2024-01-07 16:43:13 +0300

    result on 07-01-2024

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import yaml
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+with open('test_data') as file:
+    test_data = yaml.safe_load_all(file)
+    test_browser = test_data.get('browser')
+
+@pytest.fixture(scope='session')
+def browser():
+    if test_browser == 'firefox':
+        service = Service(executable_path=GeckoDriverManager().install())
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox(service=service, options=options)
+    else:
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(service=service, options=options)
+    yield driver
+    driver.quit()
